c069438e9da441b67ebf7abed707e7dc0d27eaed
justinscot <jscott65@tennessee.edu>
2019-06-11 15:20:14 -0400

    removed left over stuff in setup.py. Now it works on rhea. MUST set PATH and LD_LIBRARY_PATH to include globes

--- setup.py
+++ setup.py
@@ -4,11 +4,9 @@ import os
 import subprocess
 from os import path
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
-
-#os.chdir('./src')
 
 #Set GLB_DIR and SNOWGLOBES global variables for installation
 
@@ -16,17 +14,10 @@ GLB_DIR = subprocess.check_output('globes-config --prefix', shell=True)
 GLB_DIR = str(GLB_DIR, 'utf-8')
 SNOWGLOBES = here
 
-#print(GLB_DIR)
-
 os.environ["GLB_DIR"]= GLB_DIR
 os.environ["SNOWGLOBES"] = SNOWGLOBES
 
-#Make and Make Install the src
-#os.system('make')
-#os.system('make install')
 
-#os.chdir('..')
-#
 setup(
     name = 'SNOwGLoBES',
     version = '2.0.0',
