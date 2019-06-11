99716092127248277599a7ad9fe4d19df8ddd015
justinscot <jscott65@tennessee.edu>
2019-06-11 14:41:57 -0400

    made supernova.py importable. (i.e added a main funct)

--- supernova.py
+++ supernova.py
@@ -9,28 +9,35 @@ import sys
 import numpy as np
 from src.snowglobes import supernova, create_AEDL_file, apply_weights
 
-parser = argparse.ArgumentParser(description = 'SNOwGLoBES: public software for computing interaction rates and distributions of observed quantities for supernova burst neutrinos in common detector materials.')
-parser.add_argument('fluxname', type=str, help='Name of flux. \n (eg. livermore)')
-parser.add_argument('channelname', type=str, help='Name of channel. \n (eg. argon)')
-parser.add_argument('experimentname', type=str, help='Name of experiment. \n (eg. ar17kt)')
-parser.add_argument('--weight',action='store_true', help='Apply weighting factor. \n')
+def main(fluxname, channame, expt_config, weight):
 
-# e.g. python supernova.py livermore argon ar17kt
-#if statement for --td declaring class which is obvis diff
-args = parser.parse_args()
+    t = create_AEDL_file(fluxname, channame, expt_config)
 
-fluxname = args.fluxname
-channame = args.channelname
-expt_config = args.experimentname
-weight = args.weight
+    s = supernova(fluxname, channame, expt_config)
 
-t = create_AEDL_file(fluxname, channame, expt_config)
+    if weight:
+        print("Applying channel weighting factors to output")
+        apply_weights("")
+        apply_weights("_smeared")
+    else:
+        print("No weighting factors applied to output")
 
-s = supernova(fluxname, channame, expt_config)
+if __name__ == '__main__':
 
-if weight:
-    print("Applying channel weighting factors to output")
-    apply_weights("")
-    apply_weights("_smeared")
+    parser = argparse.ArgumentParser(description = 'SNOwGLoBES: public software for computing interaction rates and distributions of observed quantities for supernova burst neutrinos in common detector materials.')
+    parser.add_argument('fluxname', type=str, help='Name of flux. \n (eg. livermore)')
+    parser.add_argument('channelname', type=str, help='Name of channel. \n (eg. argon)')
+    parser.add_argument('experimentname', type=str, help='Name of experiment. \n (eg. ar17kt)')
+    parser.add_argument('--weight',action='store_true', help='Apply weighting factor. \n')
+
+    # e.g. python supernova.py livermore argon ar17kt
+    #if statement for --td declaring class which is obvis diff
+    args = parser.parse_args()
+
+    fluxname = args.fluxname
+    channame = args.channelname
+    expt_config = args.experimentname
+    weight = args.weight
+    main(fluxname, channame, expt_config, weight)
 else:
-    print("No weighting factors applied to output")
+    print("supernova.py is being imported into another module")
