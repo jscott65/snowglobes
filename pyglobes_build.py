7b4d36831b242149d159f6c5c6c95733248820bf
justinscot <jscott65@tennessee.edu>
2019-07-15 16:11:08 -0400

    /globes

--- pyglobes_build.py
+++ pyglobes_build.py
@@ -24,7 +24,7 @@ INCLUDES = """
 #include <ctype.h>
 """
 
-globes = '#include "{GLB_INCLUDE}/globes.h"'.format(GLB_INCLUDE='/home/vsts/work/1/s/globes/include')
+globes = '#include "{GLB_INCLUDE}/globes.h"'.format(GLB_INCLUDE='/home/vsts/work/1/s/globes/include/globes')
 
 DEFINES = """
     /* Constants */
