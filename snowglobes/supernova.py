1a28d9323439c194d7c6f40742f05e276ef64912
justinscot <jscott65@tennessee.edu>
2019-07-17 14:01:11 -0400

    add argeparse to main()

--- snowglobes/supernova.py
+++ snowglobes/supernova.py
@@ -9,8 +9,24 @@ from snowglobes.aedl import create_AEDL_file
 
 def main(fluxname, channame, expt_config, *weight):
 
+    parser = argparse.ArgumentParser(
+        description='SNOwGLoBES: public software for computing interaction rates and distributions of observed quantities for supernova burst neutrinos in common detector materials.')
+    parser.add_argument('fluxname', type=str, help='Name of flux. \n (eg. livermore)')
+    parser.add_argument('channelname', type=str, help='Name of channel. \n (eg. argon)')
+    parser.add_argument('experimentname', type=str, help='Name of experiment. \n (eg. ar17kt)')
+    parser.add_argument('--weight', action='store_true', help='Apply weighting factor. \n')
+
+    # e.g. python supernova.py livermore argon ar17kt
+    args = parser.parse_args()
+
+    fluxname = args.fluxname
+    channame = args.channelname
+    expt_config = args.experimentname
+    weight = args.weight
+
+
     chan = Channel(channame)
-    
+
     det = Detector()
 
     t = create_AEDL_file(fluxname, chan, det, expt_config)
