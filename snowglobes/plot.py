09fcf0561109bce01d822e919ce942ceb33bf4bb
justinscot <jscott65@tennessee.edu>
2019-07-24 16:04:25 -0400

    added event vs time plotter, and improved plotflux usage

--- snowglobes/plot.py
+++ snowglobes/plot.py
@@ -3,12 +3,14 @@ import os
 import pandas as pd
 import numpy as np
 
+from .interp import get_timesteps
+
 here = os.path.dirname(os.path.abspath(__file__))
 
-def get_totals(fluxname, chan, expt_config):
+def get_totals(fluxname, channels, expt_config):
 
-    totals = np.zeros(len(chan.name))
-    for i, channel in enumerate(chan.name):
+    totals = np.zeros(len(channels))
+    for i, channel in enumerate(channels):
         filepath = here + '/out/' + fluxname + '_' + channel + '_' + expt_config + '_events_smeared.dat'
 
         data = np.genfromtxt(filepath, skip_header=201)
@@ -22,7 +24,7 @@ def plotflux(fluxname, td=False, output_name=False, interactive=False):
 
     if td:
         path = here + '/fluxes/' + fluxname
-
+        print(path)
         files = os.listdir(path)
         files.sort()
 
@@ -31,10 +33,12 @@ def plotflux(fluxname, td=False, output_name=False, interactive=False):
 
         for i, file in enumerate(files):
             filepath = path + '/' + file
-
+            print(filepath)
             d = np.genfromtxt(filepath)
 
             df_step = pd.DataFrame(data=d)
+
+            #print(df_step)
 
             df_step.columns = ['energy', 'nue', 'numu', 'nutau', 'nuebar', 'numubar', 'nutaubar']
 
@@ -55,6 +59,8 @@ def plotflux(fluxname, td=False, output_name=False, interactive=False):
 
         df.columns = ['energy', 'nue', 'numu', 'nutau', 'nuebar', 'numubar', 'nutaubar']
 
+    #print(df)
+
     ax = plt.gca()
     plt.style.use('ggplot')
 
@@ -63,21 +69,22 @@ def plotflux(fluxname, td=False, output_name=False, interactive=False):
         if df['numubar'].values[3]==df['nutaubar'].values[3]:
             if df['nue'].values[3]==df['nuebar'].values[3]:
                 flavs = ['nu', 'nux', 'nuxbar']
-                df['nu'] = df['nue'] * 2
-                df['nux'] = df['numu'] * 2
-                df['nuxbar'] = df['numubar'] * 2
-                labels = [r'$\nu=\nu_{e}+\bar{\nu_{e}}$', r'$\nu_{x}=\nu_{\mu}+\bar{\nu_{\mu}}$', r'$\bar{\nu_{x}}=\bar{\nu_{\mu}}+\bar{\nu_{\tau}}$']
+                df['nu'] = df['nue']
+                df['nux'] = df['numu']
+                df['nuxbar'] = df['numubar']
+                labels = [r'$\nu=\nu_{e}=\bar{\nu_{e}}$', r'$\nu_{x}=\nu_{\mu}=\bar{\nu_{\mu}}$', r'$\bar{\nu_{x}}=\bar{\nu_{\mu}}=\bar{\nu_{\tau}}$']
             elif df['numu'].values[3]==df['nutaubar'].values[3]:
                 flavs = ['nue', 'nuebar', 'nux']
-                df['nux'] = df['numu'] * 4
-                labels = [r'$\nu_{e}$', r'$\bar{\nu_{e}}$', r'$\nu_{x}=\nu_{\mu}+\bar{\nu_{\mu}}+\nu_{\tau}+\bar{\nu_{\tau}}$']
+                df['nux'] = df['numu']
+                labels = [r'$\nu_{e}$', r'$\bar{\nu_{e}}$', r'$\nu_{x}=\nu_{\mu}=\bar{\nu_{\mu}}=\nu_{\tau}=\bar{\nu_{\tau}}$']
             else:
                 flavs = ['nue', 'nuebar', 'nux', 'nuxbar']
-                df['nux'] = df['numu'] * 2
-                df['nuxbar'] = df['numubar'] * 2
-                labels = [r'$\nu_{e}$', r'$\bar{\nu_{e}}$', r'$\nu_{x}=\nu_{\mu}+\nu_{\tau}$', r'$\bar{\nu_{x}}=\bar{\nu_{\mu}}+\bar{\nu_{\tau}}$']
+                df['nux'] = df['numu']
+                df['nuxbar'] = df['numubar']
+                labels = [r'$\nu_{e}$', r'$\bar{\nu_{e}}$', r'$\nu_{x}=\nu_{\mu}=\nu_{\tau}$', r'$\bar{\nu_{x}}=\bar{\nu_{\mu}}=\bar{\nu_{\tau}}$']
     else:
         flavs = ['nue', 'numu', 'nutau', 'nuebar', 'numubar', 'nutaubar']
+    #labels = ['nue', 'numu', 'nutau', 'nuebar', 'numubar', 'nutaubar']
         labels = [r'$\nu_{e}$', r'$\bar{\nu_{e}}$', r'$\nu_{\mu}$', r'$\bar{\nu_{\mu}}$', r'$\nu_{\tau}$', r'$\bar{\nu_{\tau}}$']
 
     for i, flav in enumerate(flavs):
@@ -112,29 +119,63 @@ def plotflux(fluxname, td=False, output_name=False, interactive=False):
 
     print(f'Saved as {f_out}')
 
+    plt.clf()
 
-def plot_rate_vs_time(fluxname, chan):
+
+def plot_rate_vs_time(fluxname, channels, expt_config, cumulative=False, log=False, interactive=False):
 
     path = here + '/fluxes/' + fluxname
 
     files = [os.path.splitext(filename)[0] for filename in os.listdir(path)]
     files.sort()
 
-    df_tot = pd.DataFrame(data=np.zeros((len(files),len(chan.name)+1)))
-    df_tot.columns = ['pb_time'] + chan.name
+    data=np.zeros((len(files),len(channels)))
+
 
     for i, file in enumerate(files):
         #fluxname = fluxname + '/' + file
 
-        d = get_totals(fluxname + '/' + file, chan, expt_config)
+        d = get_totals(fluxname + '/' + file, channels, expt_config)
+        data[i] = d
 
-        df = pd.DataFrame(data=d)
+    df = pd.DataFrame(data)
 
-        df = df.T
+    if cumulative:
+        df = df.cumsum()
 
-        df.columns = chan.name
+    df.columns = channels
 
-        df
+    timepath = path + '_timesteps.dat'
 
-        #Sum values from each value into a single dataframe
-        df_tot.at[i, chan.name] =  df
+    overall_time, pb_time = np.genfromtxt(timepath).T
+
+    df.insert(loc=0, column='pb_time', value=pb_time.tolist())
+
+    print(df)
+
+    ax = plt.gca()
+    plt.style.use('ggplot')
+
+    for i, chan in enumerate(channels):
+
+        subplot = df.plot(kind='line',x='pb_time',y=chan,ax=ax)
+
+    ax.legend(channels, loc='center right')
+
+    ax.set(xlabel='Post Bounce Time (s)', ylabel='Total Events Detected', title='Events vs Time')
+
+    caption = f'{fluxname} events_smeared.'
+
+    plt.figtext(.02, .02, caption, size='x-small')
+    if log:
+        plt.yscale('log')
+
+    ax.grid()
+
+    if interactive:
+        plt.show()
+
+    f_out = fluxname + "_events_vs_time_plot.png"
+    plt.savefig(f_out)
+
+    print(f'Saved as {f_out}')
