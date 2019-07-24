758c8a323a4da641bdbd7f3aa9c5bd1361cf3d32
justinscot <jscott65@tennessee.edu>
2019-07-24 16:34:08 -0400

    updated formatting and added comments

--- snowglobes/plot.py
+++ snowglobes/plot.py
@@ -1,11 +1,13 @@
-import matplotlib.pyplot as plt
 import os
-import pandas as pd
+
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 from .interp import get_timesteps
 
 here = os.path.dirname(os.path.abspath(__file__))
+
 
 def get_totals(fluxname, channels, expt_config):
 
@@ -28,30 +30,28 @@ def plotflux(fluxname, td=False, output_name=False, interactive=False):
         files = os.listdir(path)
         files.sort()
 
-        df = pd.DataFrame(data=np.zeros((501,7)))
+        df = pd.DataFrame(data=np.zeros((501, 7)))
         df.columns = ['energy', 'nue', 'numu', 'nutau', 'nuebar', 'numubar', 'nutaubar']
 
         for i, file in enumerate(files):
             filepath = path + '/' + file
-            print(filepath)
+            # print(filepath)
             d = np.genfromtxt(filepath)
 
             df_step = pd.DataFrame(data=d)
 
-            #print(df_step)
-
             df_step.columns = ['energy', 'nue', 'numu', 'nutau', 'nuebar', 'numubar', 'nutaubar']
 
-            #Sum values from each value into a single dataframe
-            df[['nue', 'numu', 'nutau', 'nuebar', 'numubar', 'nutaubar']] = df[['nue', 'numu', 'nutau', 'nuebar', 'numubar', 'nutaubar']] + df_step[['nue', 'numu', 'nutau', 'nuebar', 'numubar', 'nutaubar']]
+            # Sum values from each value into a single dataframe
+            df[['nue', 'numu', 'nutau', 'nuebar', 'numubar', 'nutaubar']] = df[['nue', 'numu', 'nutau', 'nuebar',
+                                                                                'numubar', 'nutaubar']] + df_step[['nue', 'numu', 'nutau', 'nuebar', 'numubar', 'nutaubar']]
 
-        #Set energy bin values
+        # Set energy bin values
         df['energy'] = df_step['energy']
-
 
     else:
 
-        path = here + '/fluxes/' + fluxname +'.dat'
+        path = here + '/fluxes/' + fluxname + '.dat'
 
         d = np.genfromtxt(path)
 
@@ -59,41 +59,47 @@ def plotflux(fluxname, td=False, output_name=False, interactive=False):
 
         df.columns = ['energy', 'nue', 'numu', 'nutau', 'nuebar', 'numubar', 'nutaubar']
 
-    #print(df)
-
     ax = plt.gca()
     plt.style.use('ggplot')
 
-    #add some logic for osc
-    if df['numu'].values[3]==df['nutau'].values[3]:
-        if df['numubar'].values[3]==df['nutaubar'].values[3]:
-            if df['nue'].values[3]==df['nuebar'].values[3]:
+    # logic to determine what to label the lines, and how many there should be
+    # based off of which flavs are the same, nux only or both nux and nuxbar.
+    # for oscillated fluxes, occasioanlly nue=nuebar thus nu = nue = nuebar
+    if df['numu'].values[3] == df['nutau'].values[3]:
+        if df['numubar'].values[3] == df['nutaubar'].values[3]:
+            if df['nue'].values[3] == df['nuebar'].values[3]:
                 flavs = ['nu', 'nux', 'nuxbar']
                 df['nu'] = df['nue']
                 df['nux'] = df['numu']
                 df['nuxbar'] = df['numubar']
-                labels = [r'$\nu=\nu_{e}=\bar{\nu_{e}}$', r'$\nu_{x}=\nu_{\mu}=\bar{\nu_{\mu}}$', r'$\bar{\nu_{x}}=\bar{\nu_{\mu}}=\bar{\nu_{\tau}}$']
-            elif df['numu'].values[3]==df['nutaubar'].values[3]:
+                labels = [r'$\nu=\nu_{e}=\bar{\nu_{e}}$', r'$\nu_{x}=\nu_{\mu}=\bar{\nu_{\mu}}$',
+                          r'$\bar{\nu_{x}}=\bar{\nu_{\mu}}=\bar{\nu_{\tau}}$']
+            elif df['numu'].values[3] == df['nutaubar'].values[3]:
                 flavs = ['nue', 'nuebar', 'nux']
                 df['nux'] = df['numu']
-                labels = [r'$\nu_{e}$', r'$\bar{\nu_{e}}$', r'$\nu_{x}=\nu_{\mu}=\bar{\nu_{\mu}}=\nu_{\tau}=\bar{\nu_{\tau}}$']
+                labels = [r'$\nu_{e}$', r'$\bar{\nu_{e}}$',
+                          r'$\nu_{x}=\nu_{\mu}=\bar{\nu_{\mu}}=\nu_{\tau}=\bar{\nu_{\tau}}$']
             else:
                 flavs = ['nue', 'nuebar', 'nux', 'nuxbar']
                 df['nux'] = df['numu']
                 df['nuxbar'] = df['numubar']
-                labels = [r'$\nu_{e}$', r'$\bar{\nu_{e}}$', r'$\nu_{x}=\nu_{\mu}=\nu_{\tau}$', r'$\bar{\nu_{x}}=\bar{\nu_{\mu}}=\bar{\nu_{\tau}}$']
+                labels = [r'$\nu_{e}$', r'$\bar{\nu_{e}}$', r'$\nu_{x}=\nu_{\mu}=\nu_{\tau}$',
+                          r'$\bar{\nu_{x}}=\bar{\nu_{\mu}}=\bar{\nu_{\tau}}$']
     else:
         flavs = ['nue', 'numu', 'nutau', 'nuebar', 'numubar', 'nutaubar']
-    #labels = ['nue', 'numu', 'nutau', 'nuebar', 'numubar', 'nutaubar']
-        labels = [r'$\nu_{e}$', r'$\bar{\nu_{e}}$', r'$\nu_{\mu}$', r'$\bar{\nu_{\mu}}$', r'$\nu_{\tau}$', r'$\bar{\nu_{\tau}}$']
+
+        labels = [r'$\nu_{e}$', r'$\bar{\nu_{e}}$', r'$\nu_{\mu}$',
+                  r'$\bar{\nu_{\mu}}$', r'$\nu_{\tau}$', r'$\bar{\nu_{\tau}}$']
 
     for i, flav in enumerate(flavs):
 
-        subplot = df.plot(kind='line',x='energy',y=flav,ax=ax)
+        subplot = df.plot(kind='line', x='energy', y=flav, ax=ax)
 
     ax.legend(labels)
 
-    ax.set(xlabel='Neutrino Energy (MeV)', ylabel=r'Fluence (neutrinos per 0.2 MeV per $cm^2$)', title='Fluence vs Energy')
+    ax.set(xlabel='Neutrino Energy (MeV)',
+           ylabel=r'Fluence (neutrinos per 0.2 MeV per $cm^2$)',
+           title='Fluence vs Energy')
 
     caption = f'{fluxname} fluence.'
 
@@ -101,9 +107,6 @@ def plotflux(fluxname, td=False, output_name=False, interactive=False):
 
     ax.grid()
 
-
-    #plt.style.use('seaborn-bright')
-    #print(plt.style.available)
     if interactive:
         plt.show()
 
@@ -131,8 +134,7 @@ def plot_rate_vs_time(fluxname, channels, expt_config, cumulative=False, log=Fal
     files = [os.path.splitext(filename)[0] for filename in os.listdir(path)]
     files.sort()
 
-    data=np.zeros((len(files),len(channels)))
-
+    data = np.zeros((len(files), len(channels)))
 
     for i, file in enumerate(files):
         #fluxname = fluxname + '/' + file
@@ -153,14 +155,14 @@ def plot_rate_vs_time(fluxname, channels, expt_config, cumulative=False, log=Fal
 
     df.insert(loc=0, column='pb_time', value=pb_time.tolist())
 
-    #print(df)
+    # print(df)
 
     ax = plt.gca()
     plt.style.use('ggplot')
 
     for i, chan in enumerate(channels):
 
-        subplot = df.plot(kind='line',x='pb_time',y=chan,ax=ax)
+        subplot = df.plot(kind='line', x='pb_time', y=chan, ax=ax)
 
     ax.legend(channels, loc='upper right')
 
